81a89f9bb93e5379f787aac404b4ba9879106b1f
Nahuel Tolosa <josetolosa98@gmail.com>
2021-11-30 00:47:43 -0300

    Implementación de Router.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import {NavBar} from './Components/NavBar/NavBar';
-import {Footer} from './Components/Footer/Footer';
-import { Header } from './Components/Header/Header';
-import { Main } from './Components/Main/Main';
+import React from 'react';
+import {NavBar} from './components/NavBar/navBar';
+import {Footer} from './components/Footer/footer';
+import { Header } from './components/Header/header';
+import { Main } from './components/Main/main';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import './Colors.css';
 
 function App() {
 
   return (
-    <div className="App">
+    <BrowserRouter>
       <NavBar />
       <Header />
-      <Main />
+      <Routes>
+        <Route path = "/" element = {<Main />}/>
+      </Routes>
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
