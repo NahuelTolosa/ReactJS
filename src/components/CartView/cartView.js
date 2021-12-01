9d94aad8d19ca4379204a838bcf76a4e6dce18ed
Nahuel Tolosa <josetolosa98@gmail.com>
2021-12-01 04:27:36 -0300

    Se creó una vista sencilla para el carrito de compras.

--- src/components/CartView/cartView.js
+++ src/components/CartView/cartView.js
@@ -1,7 +1,57 @@
+import { useContext } from 'react';
+import { cartContext } from '../../context/cartContext';
+import { BsFillTrashFill } from "react-icons/bs";
 import './cartView.scss';
 
 export const CartView = () => {
+
+    const {cart} = useContext(cartContext);
     return (
-        <h2>Carrito de compras</h2>
+        <main className='main'>
+            <h2 className='title'>Carrito de compras</h2>
+            <hr/>
+
+            <div className = 'cart-header'>
+
+                <div className = 'item'>
+                    <p>Producto</p>
+                </div>
+
+                <div className='item'>
+                    <p>Cantidad</p>
+                </div>
+
+                <div className='item'>
+                    <p>Precio</p>
+                </div>
+
+                <div className='item'>
+                    <p>Borrar</p>
+                </div>
+
+            </div>
+
+            <div className='cart-body'>
+                {
+                    cart.map((el) =>(
+                        <div key={el.id} className='cart-item'>
+                            <div className='item'>
+                                <h5>{el.name}</h5>
+                            </div>
+                            <div className='item'>
+                                <p>{el.quantity}</p>
+                            </div>
+                            <div className='item'>
+                                <p>${el.price}</p>
+                            </div>
+                            <div className='item'>
+                                <span><BsFillTrashFill /></span>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </main>
+        
     );
 }
